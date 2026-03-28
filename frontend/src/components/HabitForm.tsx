b330139b53c15f9f8e8c608913867d7b9859f0b0
Eliseo <woweg50@gmail.com>
2026-03-27 22:14:12 -0600

    test

--- frontend/src/components/HabitForm.tsx
+++ frontend/src/components/HabitForm.tsx
@@ -31,6 +31,7 @@ function HabitForm({ initialData, title }: HabitFormProps) {
     const name = formData.get("name") as string;
     const description = formData.get("description") as string;
 
+    console.log(user);
     const habitData = {
       name,
       description,
