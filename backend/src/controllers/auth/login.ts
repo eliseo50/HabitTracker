4d83323f275336e8631b16a290e175a766e5985e
Eliseo <woweg50@gmail.com>
2026-03-27 20:34:52 -0600

    fix

--- backend/src/controllers/auth/login.ts
+++ backend/src/controllers/auth/login.ts
@@ -36,7 +36,7 @@ export async function login(req: Request, res: Response) {
 
     res.cookie("session_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
